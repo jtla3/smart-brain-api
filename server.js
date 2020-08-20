3ab6d0b0335b4f4e0990774f19398a9942a98778
Mark Marchant <jtla3@yahoo.com>
2020-08-20 13:53:24 -0700

    working on endpoints

--- server.js
+++ server.js
@@ -1,9 +1,74 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('this is working');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@email.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "456",
+      name: "Sally",
+      email: "sally@email.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
 });
+
+// signin
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("sucess");
+  } else {
+    res.status(400).json("password and email do not match");
+  }
+  res.json("signin");
+});
+
+// register
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  })
+  res.json(database.users[database.users.length - 1]);
+})
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true
+      return res.json(user);
+    }
+  })
+  if (!found) {
+    res.status(404).json("user not found")
+  }
+})
 
 app.listen(3001, () => {
   console.log("app is running on port 3001");
