9e4bbc6962610010262deb5d576faa15d79be9ca
Mark Marchant <jtla3@yahoo.com>
2020-08-20 15:21:17 -0700

    got a template set up of signin, register, profile, and image endpoints working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,13 @@ const database = {
       joined: new Date(),
     },
   ],
+  login: [
+    {
+      id: "987",
+      hash: "",
+      email: "john@email.com",
+    },
+  ],
 };
 
 app.get("/", (req, res) => {
@@ -31,6 +39,19 @@ app.get("/", (req, res) => {
 
 // signin
 app.post("/signin", (req, res) => {
+  // Load hash from your password DB.
+  bcrypt.compare("apples", '$2a$10$2DDtI1.xqMsVFGPiIcoGRuQH333./ecQ41oz1r/aUVKNW9eHwXd3K', function (err, res) {
+    // res == true
+    console.log("first guess", res)
+  });
+  bcrypt.compare(
+    "veggies",
+    "$2a$10$2DDtI1.xqMsVFGPiIcoGRuQH333./ecQ41oz1r/aUVKNW9eHwXd3K",
+    function (err, res) {
+      // res = false
+      console.log("second guess", res);
+    }
+  );
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password
@@ -46,29 +67,45 @@ app.post("/signin", (req, res) => {
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
   database.users.push({
-    id: '125',
+    id: "125",
     name: name,
     email: email,
     password: password,
     entries: 0,
-    joined: new Date()
-  })
+    joined: new Date(),
+  });
   res.json(database.users[database.users.length - 1]);
-})
+});
 
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
   let found = false;
-  database.users.forEach(user => {
+  database.users.forEach((user) => {
     if (user.id === id) {
-      found = true
+      found = true;
       return res.json(user);
     }
-  })
+  });
   if (!found) {
-    res.status(404).json("user not found")
+    res.status(404).json("user not found");
   }
-})
+});
+
+// image endpoint to update entry input
+app.post("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(404).json("not found");
+  }
+});
 
 app.listen(3001, () => {
   console.log("app is running on port 3001");
